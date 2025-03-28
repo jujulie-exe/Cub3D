6aa02697115ad22ba14392154b3571c6ae3a54ab
Julie Jeannine Hunter Franco <jfranco@fu-r5-p6.s19.be>
2025-03-28 18:45:07 +0100

    ADD_VALIDITA_COLORE

--- src/parsing/add_color.c
+++ src/parsing/add_color.c
@@ -6,7 +6,7 @@
 /*   By: jfranco <jfranco@student.s19.be>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/28 16:42:35 by jfranco           #+#    #+#             */
-/*   Updated: 2025/03/28 18:28:32 by jfranco          ###   ########.fr       */
+/*   Updated: 2025/03/28 18:43:50 by jfranco          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,7 +28,9 @@ void	check_and_charge_color(t_data_maps *ptr_maps)
 		tmp++;
 	len = ft_strlen(tmp);
 	int deb;
-	if (len > 12)
+	/* aggiungere una funzione per verficare che ci siano solo
+	 * carrateri validi con la giustat quantita massimo 9 digit 3 ,*/
+	if (len > 12 || char_validy(tmp))
 		return ;
 	else
 	{
