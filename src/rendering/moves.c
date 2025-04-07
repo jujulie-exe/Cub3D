c2757ed4ccc337fa4ca481df2ca5c06d46bd6de3
Isarc-en-ciel <125564725+Isarc-en-ciel@users.noreply.github.com>
2025-04-07 18:52:05 +0000

    Init Player

--- src/rendering/moves.c
+++ src/rendering/moves.c
@@ -14,3 +14,18 @@ int dda_calcul(float *dist_x, float *dist_y)
 {
     
 }
+
+void    move_player(t_player *player)
+{
+    int speed;
+
+    speed = ?;
+    if (player->up)
+        player->y += speed;
+    if (player->down)
+        player->y -= speed;
+    if (player->left)
+        player->x -= speed;
+    if (player->right)
+        player->x += speed;
+}
--- src/rendering/player.c
+++ src/rendering/player.c
@@ -1,10 +1,23 @@
 void    init_player(t_player *player)
 {
-    player->x = window_W/2;
+    player->x = window_W/2; //mettre les vraies variables par rapport à la fenetre
     player->y = window_H/2;
 
-    player->up = false;
+    player->up = false; //voir comment on fait pour les keyhooks
     player->down = false;
     player->left = false;
     player->right = false;
+}
+
+int key_press(int keycode, t_player *player)
+{
+    if (keycode == W)
+        player->up = true;
+    if (keycode == S)
+        player->down = true;
+    if (keycode == A)
+        player->left = true;
+    if (keycode == D)
+        player->right = true;
+    return (0);
 }
